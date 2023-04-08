b6712766aaf561f365142f2e08da531abdc70a7d
dfcaixeta <dfcaixeta@gmail.com>
2023-04-08 14:42:18 -0300

    Estudo sobre a f-strings em Python.

---
+++ f_string.py
@@ -0,0 +1,16 @@
+''' Estudos sobre a formatação de strings (f-string) em Python
+Autor: Daniel Caixeta @dfcaixeta em 08.abr.23 '''
+
+# As variáveis 1
+a = 'Chicó'
+b = 'João Grilo'
+c = 'Ariano Suassuna'
+
+# As variáveis 2 - Formatação da string (f-string)
+suassuna = f'{a}'
+ariano = f'{b}'
+genio = f'{c}'
+
+# Impressão dos resultados da f-string
+print('Os personagens', ariano + ' e', suassuna, 'pertencem ao universo de', genio)
+print("")
