abc070b58edd0be51e531abc9bfe4019603c6247
dfcaixeta <dfcaixeta@gmail.com>
2023-04-15 22:59:42 -0300

    Conversão decimal para outros sistemas numéricos (bin, octa, hexa) em Python

--- dec2other.py
+++ dec2other.py
@@ -19,7 +19,9 @@ print('-' * 44)
 print('')
 
 ''' Used built-in functions bin(), oct() and hex() to convert the given
-decimal number into respective number systems.'''
+decimal number into respective number systems. A number with the prefix
+0b is considered binary, 0o is considered octal and 0x as hexadecimal.
+'''
 
 print(bin(dec), "in binary.")
 print(oct(dec), "in octal.")
