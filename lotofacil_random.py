7967ef5b7a12c17004719622de75fad6a5cb7f46
dfcaixeta <dfcaixeta@gmail.com>
2023-04-21 19:22:15 -0300

    Código randômico lotofácil - versão 2

--- lotofacil_random.py
+++ lotofacil_random.py
@@ -1,11 +1,12 @@
 '''
 Tentativa de criação de uma código para a criação de 15 números aleatórios
-baseados nas estatisticas de números quentes (hots_numb) e máxima ocorrência nos
-últimos 20 sorteios (max_occur).
+baseados nas estatisticas de números quentes (hots_numb), máxima ocorrência nos
+últimos 20 sorteios (max_occur) e números primos < 25 (primes_numb)
 
 Atribuições do programa:
-    1. Listar números quentes ... (hots_numb)
-    2. Lista números de mais ocorrências ... (max_occur)
+    1. Listar números quentes ... (hots_numb).
+    2. Listar números de mais ocorrências ... (max_occur).
+    3. Listar números primos menores que 25 ... (primes_numb).
     3. Comparar listas e separar os números sem repetição ...
     4. Gerar listas com 15 números (lista1, lista2, ...) randômicas
 
@@ -20,19 +21,21 @@ Lista atualizada manualmente através das estatísticas apresentadas pelo App.
 
 hots_numb = Números mais sorteados nos últimos sorteios (Números quentes).
 max_occur = Máxima ocorrência de números de acordo com os últimos 20 sorteios.
+primes_numb = Lista de números primos menores que 25.
 
-Autor: Daniel Caixeta - 17.abr.2023 (v.1)
+Autor: Daniel Caixeta - 19.abr.2023 (versão atualizada - Inclusão dos números primos < 25)
 '''
 
 # Parâmetros de entrada.
 
 hots_numb = [3, 4, 7, 8, 9, 10, 11, 12, 14, 15, 18, 19, 20, 21, 23, 25] 
 max_occur = [20, 25, 4, 12, 13, 23, 1, 7, 8, 11, 19, 24, 2, 10, 21, 9, 22, 3, 6, 14, 17]
+primes_numb = [2, 3, 5, 7, 11, 13, 17, 19, 23] # Parâmetro de entrada fixo (não varia)...
 
 # Combinação e ordenação das duas listas (hots_numb + max_occur -> join_list)
 
-#join_list = (sorted(set(hots_numb + max_occur)))
-join_list = (hots_numb + max_occur)
+#join_list = (sorted(set(hots_numb + max_occur + primes_numb)))
+join_list = (hots_numb + max_occur + primes_numb)
 
 # Organização das lista unida (join_list) e colocar em ordem ...
 unique_list = sorted(map(lambda x: x[1], filter(lambda x: x[1] not in
