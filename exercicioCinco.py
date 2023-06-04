5fb642eeb0ef9306c111461792abc4e3fc23dca8
dfcaixeta <dfcaixeta@gmail.com>
2023-06-04 16:37:42 -0300

    Algoritmo em Python que recebe um número inteiro e imprime seu dígito das dezenas.

---
+++ exercicioCinco.py
@@ -0,0 +1,22 @@
+''' Algoritmo em Python que recebe um número inteiro e imprime seu
+dígito das dezenas.
+
+autor: Daniel Caixeta @dfcaixeta em 04.jun.23 '''
+
+print("    ----------------------------------------------------    ")
+print("          Algoritmo que imprime o dígito da dezena          ")
+print("    ----------------------------------------------------    ")
+print()
+
+entraInt = int(input("Digite um número inteiro: "))
+
+print()
+
+resultUm = (entraInt // 10)
+resultFinal = resultUm % 10
+
+print('O dígito da dezena é',resultFinal)
+
+print()
+
+# EOF
