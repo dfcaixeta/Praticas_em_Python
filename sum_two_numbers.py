02c2c980e2482de72efa7d6c00d46c39c7a3828a
dfcaixeta <dfcaixeta@gmail.com>
2023-06-06 12:28:18 -0300

    Algoritmo que soma dois números.

---
+++ sum_two_numbers.py
@@ -0,0 +1,17 @@
+''' Algoritmo que soma dois números.
+Autor: Daniel Caixeta @dfcaixeta em 06.jun.23. '''
+
+# Store input numbers
+
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+
+sum = float(num1) + float(num2)
+
+# Display the sum
+
+print('The sum of {0} and {1} is {2:.2f}'.format(num1, num2, sum))
+
+# EOF
