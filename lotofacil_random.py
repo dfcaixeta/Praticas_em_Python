c47ecbbca5d763b3a5f1d8271d0c4f8a50c0fa2b
dfcaixeta <dfcaixeta@gmail.com>
2023-12-15 12:17:22 -0300

    Lofofácil v.3 - Números pares e ímpares.

--- lotofacil_random.py
+++ lotofacil_random.py
@@ -1,7 +1,8 @@
 '''
 Tentativa de criação de uma código para a criação de 15 números aleatórios
 baseados nas estatisticas de números quentes (hots_numb), máxima ocorrência nos
-últimos 20 sorteios (max_occur) e números primos < 25 (primes_numb)
+últimos 20 sorteios (max_occur), números primos < 25 (primes_numb), série de fibonacci
+<= 25 (fibon_numbs), ...
 
 Atribuições do programa:
     1. Listar números quentes ... (hots_numb).
@@ -22,21 +23,26 @@ Lista atualizada manualmente através das estatísticas apresentadas pelo App.
 hots_numb = Números mais sorteados nos últimos sorteios (Números quentes).
 max_occur = Máxima ocorrência de números de acordo com os últimos 20 sorteios.
 primes_numb = Lista de números primos menores que 25.
+fibon_numb = Números da série de Fibonacci <= 25.
+pairs_numb = Números pares <= 25.
+odd_numb = Números ímpares <= 25.
 
-Autor: Daniel Caixeta - 19.abr.2023 (versão atualizada - Inclusão dos números primos < 25)
-'''
+Autor: Daniel Caixeta - 15.dez.2023 (versão atualizada - Inclusão dos números pares e 
+ímpares <= 25) '''
 
 # Parâmetros de entrada.
 
-hots_numb = [1, 5, 6, 8, 9, 10, 11, 14, 15, 16, 17, 18, 20, 21, 23, 24] 
+hots_numb = [1, 3, 4, 5, 6, 8, 9, 10, 11, 13, 14, 18, 20, 24] # Atualizado em 15.dez.23
 max_occur = [20, 25, 4, 12, 13, 23, 1, 7, 8, 11, 19, 24, 2, 10, 21, 9, 22, 3, 6, 14, 17]
 primes_numb = [2, 3, 5, 7, 11, 13, 17, 19, 23] # Parâmetro de entrada fixo (não varia)...
-fibonacci_numb = [1, 2, 3, 5, 8, 13, 21]
+fibon_numb = [1, 2, 3, 5, 8, 13, 21]
+pairs_numb = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24] # Números pares da série.
+odd_numb = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25] # Números ímpares da série.
 
-# Combinação e ordenação das duas listas (hots_numb + max_occur -> join_list)
+# Combinação e ordenação das listas acima (hots_numb + max_occur ... -> join_list)
 
-#join_list = (sorted(set(hots_numb + max_occur + primes_numb)))
-join_list = (hots_numb + max_occur + primes_numb)
+# join_list = (sorted(set(hots_numb + max_occur + primes_numb + pairs_numb)))
+join_list = (hots_numb + max_occur + primes_numb + fibon_numb + pairs_numb + odd_numb)
 
 # Organização das lista unida (join_list) e colocar em ordem ...
 unique_list = sorted(map(lambda x: x[1], filter(lambda x: x[1] not in
@@ -60,22 +66,22 @@ random.shuffle(unique_list)
 
 random_list1 = random.sample(unique_list, 15)
 random_list2 = random.sample(unique_list, 15)
-random_list3 = random.sample(unique_list, 15)
-random_list4 = random.sample(unique_list, 15)
-random_list5 = random.sample(unique_list, 15)
-random_list6 = random.sample(unique_list, 15)
-random_list7 = random.sample(unique_list, 15)
+#random_list3 = random.sample(unique_list, 15)
+#random_list4 = random.sample(unique_list, 15)
+#random_list5 = random.sample(unique_list, 15)
+#random_list6 = random.sample(unique_list, 15)
+#random_list7 = random.sample(unique_list, 15)
 
 # Impressão das interações processadas.
 
-print('Resultado da interação para {7} interações:')
+print('Resultado da interação para {2} interações:')
 print(f'Resultado {1}', sorted(random_list1))
 print(f'Resultado {2}', sorted(random_list2))
-print(f'Resultado {3}', sorted(random_list3))
-print(f'Resultado {4}', sorted(random_list4))
-print(f'Resultado {5}', sorted(random_list5))
-print(f'Resultado {6}', sorted(random_list6))
-print(f'Resultado {7}', sorted(random_list7))
+#print(f'Resultado {3}', sorted(random_list3))
+#print(f'Resultado {4}', sorted(random_list4))
+#print(f'Resultado {5}', sorted(random_list5))
+#print(f'Resultado {6}', sorted(random_list6))
+#print(f'Resultado {7}', sorted(random_list7))
 
 print('')
 
