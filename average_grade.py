10fc1e3a4f5074d2c9ad77a1f069fc61bd7282c6
Daniel Caixeta <dfcaixeta@gmail.com>
2024-03-23 22:41:16 -0300

    Algoritmo em Python que informa [...]  estrutura condicional IF e operador lógico OR. (Versão em inglês).

--- average_grade.py
+++ average_grade.py
@@ -1,8 +1,10 @@
 ''' Algoritmo em Python que informa se o aluno conquistou o certificado
-ou não, usando a estrutura condicional (if) e operadores lógicos.
+ou não, usando a estrutura condicional IF e operador lógico OR. (Versão em inglês).
+
 Autor: Daniel Caixeta @dfcaixeta 01.jul.2023 '''
 
-# Input static variable -> data
+# Input static variable -> data 
+
 average_grade = 'A'
 final_score = 1400
 
