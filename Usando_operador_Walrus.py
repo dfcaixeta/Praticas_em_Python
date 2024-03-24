cb73c020e70d17c3c5315650831b8376c4e7cb25
Daniel Caixeta <dfcaixeta@gmail.com>
2024-03-23 22:34:15 -0300

    Usando o operador Walrus

--- Usando_operador_Walrus.py
+++ Usando_operador_Walrus.py
@@ -1,16 +1,32 @@
 # Usando o operador Walrus
 
-print()
+''' Com esse operador, é possível atribuir um valor a uma variável dentro de uma expressão.
+Isso pode simplificar o código e deixá-lo mais legível, principalmente em casos em que é necessário
+verificar uma condição antes de atribuir um valor a uma variável. '''
+
+# @utor: Daniel Caixeta - dfcaixeta - Data: 23.mar.2024
+
+print('-' * 30)
 print("Usando o operador Walrus ...")
+print('-' * 30)
+
 print()
+
+# Dados de entrada ...
+funcionario = input("Digite o nome do funcionário: ")
+
+# Usando o operador walrus :=
 if (vendas:=float(input("Vendas do funcionário R$: "))) > 1000:
     bonus = 0.05 * vendas
 
+    print()
+    print(f"O bônus recebido pelo funcionário {funcionario} é de 'R$: {round(bonus, 2)}' pelas vendas.")
+
 else:
     bonus = 0
+    print()
+    print(f'O funcionário {funcionario} não recebeu bônus por vendas!')
 
-print()
-print(f"O bônus recebido pelo funcionário é de 'R$: {bonus}' pelas vendas.")
 print()
 
 # EOF
